4d0091689f70d8d3e778bad0269264b0589bcd66
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2024-01-10 09:54:41 +0900

    no message

---
+++ DPFM_API_Caller/sql.go
@@ -0,0 +1,120 @@
+package dpfm_api_caller
+
+import (
+	dpfm_api_input_reader "data-platform-api-contract-doc-reads-rmq-kube/DPFM_API_Input_Reader"
+	dpfm_api_output_formatter "data-platform-api-contract-doc-reads-rmq-kube/DPFM_API_Output_Formatter"
+	"fmt"
+	"github.com/latonaio/golang-logging-library-for-data-platform/logger"
+)
+
+func (c *DPFMAPICaller) readSqlProcess(
+	input *dpfm_api_input_reader.SDC,
+	output *dpfm_api_output_formatter.SDC,
+	accepter []string,
+	errs *[]error,
+	log *logger.Logger,
+) interface{} {
+	var headerDoc *[]dpfm_api_output_formatter.HeaderDoc
+	var itemDoc *[]dpfm_api_output_formatter.ItemDoc
+
+	for _, fn := range accepter {
+		switch fn {
+		case "HeaderDoc":
+			func() {
+				headerDoc = c.HeaderDoc(input, output, errs, log)
+			}()
+		case "ItemDoc":
+			func() {
+				itemDoc = c.ItemDoc(input, output, errs, log)
+			}()
+		}
+	}
+
+	data := &dpfm_api_output_formatter.Message{
+		HeaderDoc: headerDoc,
+		ItemDoc:   itemDoc,
+	}
+
+	return data
+}
+
+func (c *DPFMAPICaller) HeaderDoc(
+	input *dpfm_api_input_reader.SDC,
+	output *dpfm_api_output_formatter.SDC,
+	errs *[]error,
+	log *logger.Logger,
+) *[]dpfm_api_output_formatter.HeaderDoc {
+	where := "WHERE 1 = 1"
+
+	if input.HeaderDoc.Contract != nil {
+		where = fmt.Sprintf("%s\nAND Contract = %d", where, *input.HeaderDoc.Contract)
+	}
+	if input.HeaderDoc.DocType != nil && len(*input.HeaderDoc.DocType) != 0 {
+		where = fmt.Sprintf("%s\nAND DocType = '%v'", where, *input.HeaderDoc.DocType)
+	}
+	if input.HeaderDoc.DocIssuerBusinessPartner != nil && *input.HeaderDoc.DocIssuerBusinessPartner != 0 {
+		where = fmt.Sprintf("%s\nAND DocIssuerBusinessPartner = %v", where, *input.HeaderDoc.DocIssuerBusinessPartner)
+	}
+	groupBy := "\nGROUP BY Contract, DocType, DocIssuerBusinessPartner "
+
+	rows, err := c.db.Query(
+		`SELECT
+    Contract, DocType, MAX(DocVersionID), DocID, FileExtension, FileName, FilePath, DocIssuerBusinessPartner
+		FROM DataPlatformMastersAndTransactionsMysqlKube.data_platform_contract_header_doc_data
+		` + where + groupBy + `;`)
+	if err != nil {
+		*errs = append(*errs, err)
+		return nil
+	}
+	defer rows.Close()
+
+	data, err := dpfm_api_output_formatter.ConvertToHeaderDoc(rows)
+	if err != nil {
+		*errs = append(*errs, err)
+		return nil
+	}
+
+	return data
+}
+
+func (c *DPFMAPICaller) ItemDoc(
+	input *dpfm_api_input_reader.SDC,
+	output *dpfm_api_output_formatter.SDC,
+	errs *[]error,
+	log *logger.Logger,
+) *[]dpfm_api_output_formatter.ItemDoc {
+	where := "WHERE 1 = 1"
+
+	if input.HeaderDoc.Contract != nil {
+		where = fmt.Sprintf("%s\nAND Contract = %d", where, *input.HeaderDoc.Contract)
+	}
+	if input.HeaderDoc.ItemDoc.ContractItem != nil {
+		where = fmt.Sprintf("%s\nAND ContractItem = %d", where, *input.HeaderDoc.ItemDoc.ContractItem)
+	}
+	if input.HeaderDoc.ItemDoc.DocType != nil {
+		where = fmt.Sprintf("%s\nAND DocType = '%v'", where, *input.HeaderDoc.ItemDoc.DocType)
+	}
+	if input.HeaderDoc.ItemDoc.DocIssuerBusinessPartner != nil {
+		where = fmt.Sprintf("%s\nAND DocIssuerBusinessPartner = %v", where, *input.HeaderDoc.ItemDoc.DocIssuerBusinessPartner)
+	}
+	groupBy := "\nGROUP BY Contract, ContractItem, DocType, DocIssuerBusinessPartner "
+
+	rows, err := c.db.Query(
+		`SELECT
+    Contract, ContractItem, DocType, MAX(DocVersionID), DocID, FileExtension, FileName, FilePath, DocIssuerBusinessPartner
+		FROM DataPlatformMastersAndTransactionsMysqlKube.data_platform_contract_item_doc_data
+		` + where + groupBy + `;`)
+	if err != nil {
+		*errs = append(*errs, err)
+		return nil
+	}
+	defer rows.Close()
+
+	data, err := dpfm_api_output_formatter.ConvertToItemDoc(rows)
+	if err != nil {
+		*errs = append(*errs, err)
+		return nil
+	}
+
+	return data
+}
